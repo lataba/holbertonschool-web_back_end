dd1bc225ddb8b0f98793782c9d7a8238f1211201
lataba <ltballeste@gmail.com>
2023-08-31 20:24:32 -0700

    Task3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for 3-to_string.py
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Method that takes a float n as argument and returns the string
+    representation of the float.
+    """
+    return str(n)
