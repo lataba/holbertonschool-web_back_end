48ad748058e877cc0189e760695ba5b08452847d
lataba <ltballeste@gmail.com>
2023-08-31 20:36:03 -0700

    Task5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module for 5-sum_list.py
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Takes a list input_list of floats as argument and returns their sum"""
+    return sum(input_list)
