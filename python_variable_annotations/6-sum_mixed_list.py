f989133c62045f34b0b4fae53af72373aae19050
lataba <ltballeste@gmail.com>
2023-08-31 20:40:45 -0700

    Task6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for 6-sum_mixed_list.py
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Returns the sum of a list of floats and integers
+    """
+    return sum(mxd_lst)
