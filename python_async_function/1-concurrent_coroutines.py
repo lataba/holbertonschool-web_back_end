669ab9baea7486cd0928ae76305d04baf746b007
lataba <ltballeste@gmail.com>
2023-09-03 15:50:55 -0700

    Task1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,36 @@
+#!/bin/usr/env python3
+"""
+Module for task 1-concurrent_coroutines.py
+"""
+
+import asyncio
+from typing import List
+from heapq import heappush, heappop
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronously spawns `n` tasks to wait for random
+    delays up to `max_delay` and returns a list of the
+    delays in ascending order.
+    """
+    delays = []
+    delay_queue = []
+
+    async def wait_and_store_delay():
+        """
+        Asynchronously waits for a random
+        delay and stores it in a priority queue.
+        """
+        delay = await wait_random(max_delay)
+        heappush(delay_queue, delay)
+
+    tasks = [wait_and_store_delay() for _ in range(n)]
+    await asyncio.gather(*tasks)
+
+    while delay_queue:
+        delays.append(heappop(delay_queue))
+
+    return delays
