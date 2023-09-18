10b52e9b8b2260b3a5eff5d70ceefae15803ea50
lataba <ltballeste@gmail.com>
2023-09-17 17:30:42 -0700

    Tasks 0-12

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const dataArray = [];
+  for (const idx of array) {
+    dataArray.push(appendString + idx);
+  }
+
+  return dataArray;
+}
