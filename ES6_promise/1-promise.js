2ac469df2c8be5fc0008371bdce385d78999a7e7
lataba <ltballeste@gmail.com>
2023-10-13 17:57:48 -0700

    Task1, fix eslint

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,12 +1,10 @@
 export default function getFullResponseFromAPI(success) {
-const myPromise = new Promise((resolve, reject) => {
+  const myPromise = new Promise((resolve, reject) => {
     if (success) {
-    resolve(
-        { status: 200, body: 'Success' },
-    );
+      resolve({ status: 200, body: "Success" });
     } else {
-    reject(new Error('The fake API is not working currently'));
+      reject(new Error("The fake API is not working currently"));
     }
-});
-return myPromise;
+  });
+  return myPromise;
 }
